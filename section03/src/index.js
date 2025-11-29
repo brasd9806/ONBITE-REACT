ab6f9a813002b7491a116a04eecc0731810049ed
brasd9806 <ahemswl9806@naver.com>
2025-11-29 18:52:31 +0900

    Node.js라이브러리 사용하기(randomcolor)

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,5 +1,11 @@
 import mul, {add, sub} from "./math.js";
 
-console.log(add(1,2));
-console.log(sub(1,2));
-console.log(mul(2,3));
+// npmjs  >> node.js의 라이브러리를 사용할 수 있는 사이트 
+import randomColor from "randomcolor"; // 라이브러리를 가져올때는 라이브러리의 이름만 명시하면됨
+
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1,2));
+// console.log(sub(1,2));
+// console.log(mul(2,3));
