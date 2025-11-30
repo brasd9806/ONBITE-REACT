29a5e6342fc96e4f8952d26f2a7af73728c7cdf7
brasd9806 <ahemswl9806@naver.com>
2025-11-30 18:57:41 +0900

    State로 상태관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,28 +1,39 @@
 import './App.css'
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import Button from './components/Button';
+import {useState} from "react";
 
 // App 컴포넌트는 뿌리역할 하는 부모 컴포넌트임
 function App() {
 
-  const buttonProps={
-    text : "메일",
-    color:"red",
-    a: 1,
-    b: 2,
-    c: 3,
-  }
+  //useState(값, 함수) 첫번째: 현재값, 두번째:상태를 변화시키는 함수
+  const [count, setCount] = useState(0); 
+  const [light, setLight] = useState("OFF");
 
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"}/>
-      <Button text={"블로그"}>
+        {/* Button.jsx 실습 내용
+         <Button {...buttonProps} />
+        <Button text={"카페"}/>
+        <Button text={"블로그"}>
         <div>자식 요소</div>
         <Header />
-      </Button>
+        </Button> */}
+      <div>
+        <h1>{light}</h1>
+        <button onClick={() => {
+          setLight(light === "ON" ? "OFF" : "ON");
+        }}>
+          {light === "ON" ? "끄기" : "켜기"}
+          </button>
+      </div>
+      
+      <div>
+        <h1>{count}</h1>
+        <button onClick={()=>{
+          setCount(count+1);
+        }}>
+        +
+        </button>
+      </div>
     </>
   );
 };
