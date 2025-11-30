5d95b09b74ade70ea75b1861ed87df1705532d94
brasd9806 <ahemswl9806@naver.com>
2025-11-30 18:00:04 +0900

    JSX로 UI표현하기

--- section05/src/components/Main.jsx
+++ section05/src/components/Main.jsx
@@ -1,9 +1,35 @@
+import "./Main.css";
+
+// JSX 주의 사항
+// 1. 중괄호 내부에는 자바스크립트 표현식만 넣을 수 있다. 조건문이나 반복문은 쓸 수 없음
+// 2. 숫자, 문자열, 배열 값만 렌더링 된다
+// 3. 모든 태그는 닫혀있어야 한다.
+// 4. 최상위 태그는 반드시 하나여야 한다
 const Main = () => {
-    return (
-        <main>
-            <h1>main</h1>
-        </main>
-    );
+    const user = {
+        name: "이고래",
+        isLogin: false,
+    };
+
+    // 아래와 똑같이 렌더링 됨
+    if(user.isLogin) {
+        return <div style={{
+            backgroundColor: "red",
+            borderBottom: "5px solid blue",
+        }}>로그아웃</div>;
+    }else {
+        return <div className="logout">로그인</div>;
+    }
+
+    // return (
+    //     <>
+    //         {user.isLogin ? (
+    //             <div>로그아웃</div>
+    //         ) : (
+    //             <div>로그인</div>
+    //         )}
+    //     </>
+    // );
 };
 
 export default Main;
