8e6557309c17cf63559f81daf5563911436c8ae1
brasd9806 <ahemswl9806@naver.com>
2025-11-30 17:43:09 +0900

    React 컴포넌트

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
+
+// App 컴포넌트는 뿌리역할 하는 부모 컴포넌트임
 function App() {
-
   return (
     <>
-      <h1>안녕 리액트!</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default App;
---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,11 @@
+// 컴포넌트를 만들때는 꼭! 맨앞글자는 대문자로 해야함.
+// 자식 컴포넌트
+const Header = () => {
+  return(
+    <header>
+      <h1>headers</h1>
+    </header>
+  );
+};
+
+export default Header;
