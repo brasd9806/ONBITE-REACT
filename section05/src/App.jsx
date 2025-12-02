c5bbc8341e783a5c21fc669113600e3b18eaace7
brasd9806 <ahemswl9806@naver.com>
2025-12-02 20:04:46 +0900

    State로 사용자 입력 관리하기1(회원가입 폼 만들기)

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,7 @@
 import './App.css'
 import {useState} from "react";
 
-import Bulb from './components/Bulb';
-import Counter from './components/Counter';
-
+import Register from "./components/Register";
 
 
 // App 컴포넌트는 뿌리역할 하는 부모 컴포넌트임
@@ -16,16 +14,14 @@ function App() {
   return (
     <>
         {/* Button.jsx 실습 내용
-         <Button {...buttonProps} />
+        <Button {...buttonProps} />
         <Button text={"카페"}/>
         <Button text={"블로그"}>
         <div>자식 요소</div>
         <Header />
         </Button> */}
-      <Bulb />
-      
-      <Counter />
-      
+
+        <Register />
     </>
   );
 };
