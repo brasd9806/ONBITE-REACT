12897157e5d9b7ef00441c6c1ac9d04656603f9d
brasd9806 <ahemswl9806@naver.com>
2025-12-02 19:45:12 +0900

    State와 Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import {useState} from "react";
 
+import Bulb from './components/Bulb';
+import Counter from './components/Counter';
+
+
+
 // App 컴포넌트는 뿌리역할 하는 부모 컴포넌트임
 function App() {
 
   //useState(값, 함수) 첫번째: 현재값, 두번째:상태를 변화시키는 함수
-  const [count, setCount] = useState(0); 
-  const [light, setLight] = useState("OFF");
+   
+  
 
   return (
     <>
@@ -17,23 +22,10 @@ function App() {
         <div>자식 요소</div>
         <Header />
         </Button> */}
-      <div>
-        <h1>{light}</h1>
-        <button onClick={() => {
-          setLight(light === "ON" ? "OFF" : "ON");
-        }}>
-          {light === "ON" ? "끄기" : "켜기"}
-          </button>
-      </div>
+      <Bulb />
       
-      <div>
-        <h1>{count}</h1>
-        <button onClick={()=>{
-          setCount(count+1);
-        }}>
-        +
-        </button>
-      </div>
+      <Counter />
+      
     </>
   );
 };
