b5b2e312606871a46d1df261bd23b6e18e6f68dd
brasd9806 <ahemswl9806@naver.com>
2025-12-03 22:16:14 +0900

    회원가입 폼(State, useRef)

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,4 +1,5 @@
-import {useState} from 'react';
+import {useState, useRef} from 'react';
+// useState : 값이 변하면 재렌더링함.  / useRef: 값이 변경되어도 재렌더링 하지 않음
 
 // 간단한 회원가입 폼
 // 1. 이름
@@ -15,10 +16,13 @@ const Register = () => {
         bio: "",
     });
 
+    const countRef = useRef(0);
+    const inputRef = useRef();
 
     const onChange = (e) => {
         // console.log(e);
-
+        countRef.current++;
+        console.log(countRef.current);
         setInput({
             ...input,
             [e.target.name] : e.target.value,
@@ -32,12 +36,22 @@ const Register = () => {
     //         name : e.target.value
     //     })
     // }
+
+    const onSubmit = () => {
+        if(input.name === "") {
+            // 이름을 입력하는 DOM 요소 포커스
+            inputRef.current.focus();
+        }
+    };
     
 
     return (
         <div>
+            
             <div>
-                <input value={input.name} 
+                <input 
+                    ref={inputRef}
+                    value={input.name} 
                     name="name"
                     onChange={onChange} 
                     placeholder={"이름"} />
@@ -62,6 +76,8 @@ const Register = () => {
             <div>
                 <textarea value={input.bio} name='bio' onChange={onChange}/>
             </div>
+
+            <button onClick={onSubmit}>제출</button>
         </div>
     )
 }
