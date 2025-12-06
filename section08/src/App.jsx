f6538394c4c72aa2ff8ab8dc53c3f78d17441871
brasd9806 <ahemswl9806@naver.com>
2025-12-06 16:59:10 +0900

    TodoList 프로젝트 UI 구현

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
 import './App.css'
+import Header from './components/Header';
+import List from './components/List';
+import Editor from './components/Editor';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      TodoList
-    </>
+    <div className='App'>
+      <Header />
+      <Editor />
+      <List />
+    </div>
   )
 }
 
