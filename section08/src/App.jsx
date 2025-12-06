fa909c0a8ff098e492baf687fd7707e8b24702cb
brasd9806 <ahemswl9806@naver.com>
2025-12-06 17:04:41 +0900

    TodoList 기능 구현하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,8 +2,35 @@ import './App.css'
 import Header from './components/Header';
 import List from './components/List';
 import Editor from './components/Editor';
+import {useState} from 'react';
+
+// 임시데이터
+const mockData = [
+  {
+    id:0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id:1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id:2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+]
+
 
 function App() {
+
+
+  const [todos, setTodos] = useState(mockData);
 
   return (
     <div className='App'>
