c5994dc3e3dbc6add7da9c1865659b928ac180d1
brasd9806 <ahemswl9806@naver.com>
2025-12-06 16:14:52 +0900

    useEffect로 라이프사이클 제어하기

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,21 +1,44 @@
 import './App.css';
 import Viewer from './components/Viewer';
 import Controller from './components/Controller';
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useRef} from 'react';
+import Even from './components/Even';
 
 function App() {
 
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
 
-  // useEffect는 count 값이 바뀔때마다 첫번째 인자로 전달한
-  // 콜백함수를 실행시키게 됨
-  useEffect(()=>{
-    console.log(`count: ${count} / input: ${input}`);
-  }, [count, input]);
-  // 의존성 배열
-  // dependency array
-  // deps
+  const isMount = useRef(false);
+
+// 라이프 사이클 :
+// 1. 마운트 : 탄생  
+// 최초로 한번만 실행하고 싶을 때, deps로 빈배열을 전달하면 됨
+useEffect(()=> {
+  console.log("mount");
+}, []);
+
+// 2. 업데이트 : 변화, 리렌더링
+// 배열 생략
+useEffect(()=> {
+  if(!isMount.current){
+    isMount.current = true;
+    return;
+  }
+
+  console.log("update");
+});
+
+// 3. 언마운트 : 죽음
+// Even.jsx 컴포넌트 참고
+
+
+
+  // ★★ useEffect는 count 값이 바뀔때마다 첫번째 인자로 전달한 콜백함수를 실행시키게 됨
+  // 의존성 배열 dependency array(deps)
+  // useEffect(()=>{
+  //   console.log(`count: ${count} / input: ${input}`);
+  // }, [count, input]);
 
 
   const onClickButton = (value) => {
@@ -32,6 +55,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton}/>
---
+++ section07/src/components/Even.jsx
@@ -0,0 +1,16 @@
+import {useEffect} from "react";
+
+const Even = () => {
+    useEffect(()=> {
+
+        // 클린업 or 정리함수
+        // 정리함수는 useEffect가 끝날때 실행이 됨
+        return () => {
+            console.log("unmount");
+        };
+    }, []);
+
+    return <div>짝수입니다</div>
+};
+
+export default Even;
