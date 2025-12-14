73e784f04bf752212f510e607a39eb4ce19bac24
brasd9806 <ahemswl9806@naver.com>
2025-12-14 19:28:16 +0900

    todo리스트 useState -> useReducer로 변경

--- section09/src/App.jsx
+++ section09/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import Header from './components/Header';
 import List from './components/List';
 import Editor from './components/Editor';
-import {useState, useRef} from 'react';
+import {useState, useRef, useReducer} from 'react';
 import Exam from './components/Exam';
 
 // 임시데이터
@@ -27,50 +27,65 @@ const mockData = [
   },
 ]
 
+function reducer(state, action) {
+  switch(action.type){
+    case 'CREATE' : return [action.data, ...state];
+    case 'UPDATE' : return state.map((item) => 
+      item.id === action.targetId 
+        ? {...item, isDone: !item.isDone}
+        : item
+    );
+    case 'DELETE' : return state.filter((item) => 
+      item.id !== action.targetId
+    );
+    default:
+      return state;
+  }
+}
+
 
 function App() {
 
-  const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
   const idRef = useRef(3);
 
   const onCreate = (content) => {
-    const newTodo = {
-      id: idRef.current++,
-      isDone: false,
-      content: content,
-      date: new Date().getTime(),
-    }
-
-    setTodos([newTodo,...todos]);
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      }
+    })
   };
 
   // 체크박스 수정 기능
   const onUpdate = (targetId) => {
-    // todos State의 값들 중에
-    // targetId와 일치하는 id를 갖는 투두 아이템의 isDone 변경
-
-    // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
-    setTodos(todos.map((todo)=> 
-      todo.id === targetId 
-        ? {...todo, isDone:!todo.isDone} : todo));
+      dispatch({
+        type:"UPDATE",
+        targetId: targetId
+      })
   };
 
 
   // 삭제 기능
   const onDelete = (targetId) => {
-    // 인수 : todos 배열에서 targetId와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
-    // 삭제대상이 아닌 요소만 필터링하도록
-    setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      targetId: targetId
+    })
   }
 
   return (
     <div className='App'>
-      <Exam />
-      {/* <Header />
+      {/* <Exam /> */}
+      <Header />
       <Editor onCreate={onCreate}/>
       <List todos={todos} 
             onUpdate={onUpdate}
-            onDelete={onDelete}/> */}
+            onDelete={onDelete}/>
     </div>
   )
 }
